4d4d75755fc40ef6ffa978310bb262bfdc08de11
GiadaTonioni <giada.tonioni@gmail.com>
2023-05-17 16:50:14 +0200

    es_svolto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
+//richiesta km
 let km = parseInt(prompt ('quanti km devi percorrere?'));
 console.log (km);
 
-let age = parseInt(prompt('quanti anni hai?'));
+//richiesta età
+let age = parseInt(prompt('quanti hanni hai?'));
 console.log (age);
+
+//dichiarazione prezzo
+let prezzo = 0.21;
+console.log(prezzo);
+
+//costo biglietto normale
+let risultato = km * prezzo;
+console.log (risultato);
+
+//variazione costo biglietto se minore di 18 o maggiore di 65 anni
+if (age <= 17) {
+    risultato = (km * prezzo - 20 / 100)
+    console.log (risultato)
+} else if (age >= 65) {
+    risultato = (km * prezzo - 40 / 100)
+    console.log (risultato)
+}
+
+//risultato a due decimali
+risultato = risultato.toFixed(2)
+console.log (risultato)
+
+//risultato a video
+document.getElementById('prezzo_biglietto').innerHTML = `${risultato} €`
+
 
